be47cd7a0f19d42feb021d3a12afe6978d2d3dd1
the25yahya <114318659+the25yahya@users.noreply.github.com>
2024-07-02 02:19:56 +0200

    added user login

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -15,11 +15,33 @@ exports.register = async ( req, res ) => {
         if (error) {
             return res.status(500).json({error: error.message})
         }
-        console.log( results.insertId );
+        console.log( results );
         const token = jwt.sign({id: results.insertId}, config.jwtSecret, {expiresIn: '1h'})
         res.status(201).json({token})
       })
     } catch (error) {
         res.status(500).json({error: error.message})
+    }
+}
+
+exports.login = async (req, res) => {
+    try{
+        const { email , password } = req.body;
+
+        const sql = 'SELECT * FROM USERS WHERE email = ?'
+        const values = email
+
+        connection.query(sql, values, async(error, results)=>{
+            if (error) {
+                return res.status(500).json({ error: error.message})
+            }
+            if (results.length === 0 || !(await bcrypt.compare(password, results[0].password))) {
+                return res.status(401).json({error: "invalid credentials"})
+            }
+            const token = jwt.sign({id: results[0].id},config.jwtSecret,{expiresIn: '1h'});
+            res.status(200).json( { token } )
+        });
+    } catch (error){
+        res.status(500).json({error: error.message});
     }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userControllers');
 
 
 router.post('/register', userController.register)
+router.post('/login', userController.login)
 
 module.exports =router;
